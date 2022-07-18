becd161cee64e06c4ee7a61632a4464285154bc3
fs8vsx59h <106460965+fs8vsx59h@users.noreply.github.com>
2022-07-18 15:19:29 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-docker run -it \
---name=dts \
---network=host \
---mount type=bind,source=/home/izumo/DTS,target=/root/.klei/DoNotStarveTogether \
-poormancontainer/poormandts:20020704v2 ./run-master-and-caves.sh
