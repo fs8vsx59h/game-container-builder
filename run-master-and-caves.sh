8fbaaf27f177b9c6efbe10e7e4adb59c92d7caef
fs8vsx59h <106460965+fs8vsx59h@users.noreply.github.com>
2022-10-25 11:21:18 +0800

    Delete run-master-and-caves.sh

--- run-master-and-caves.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nohup /dts/bin64/dontstarve_dedicated_server_nullrenderer_x64 -console -cluster MyDediServer -shard Master & 
-./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster MyDediServer -shard Caves
